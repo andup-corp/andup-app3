e9b20e5909c3ea5e75c79b77ec89a2b2c857b5d7
andup-corp <otani_r@andup-net.com>
2025-10-30 20:33:30 +0900

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "andup-app3",
+  description: "andup向けIT導入補助金対応ツール（試作）",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="ja">
+      <body>{children}</body>
+    </html>
+  );
+}
